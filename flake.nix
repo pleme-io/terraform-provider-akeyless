b0dee16b225df62d35e759966e9706eea37afa74
drzzln <drzzln@protonmail.com>
2026-03-15 21:09:10 -0400

    refactor: use substrate repo-flake.nix universal builder
    Replaces bespoke flake.nix boilerplate with declarative config via
    substrate's repo-flake.nix. All build logic, devShell tooling, and
    meta attributes derived from (language, builder) dispatch.

--- flake.nix
+++ flake.nix
@@ -10,24 +10,16 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, substrate, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs { inherit system; };
-      mkGoTool = (import "${substrate}/lib/go-tool.nix").mkGoTool;
-    in {
-      packages.default = mkGoTool pkgs {
-        pname = "terraform-provider-akeyless";
-        version = "0.0.0-dev";
-        src = self;
-        proxyVendor = true;
-        vendorHash = "sha256-ItO75dB0d4zH09IxNyoG1gJ0NmLge7ml/hFRT0hXsJE=";
-        description = "Akeyless Terraform Provider - manage Akeyless resources via Terraform";
-        homepage = "https://github.com/pleme-io/terraform-provider-akeyless";
-        license = pkgs.lib.licenses.mpl20;
-      };
-
-      devShells.default = pkgs.mkShellNoCC {
-        packages = with pkgs; [ go gopls gotools ];
-      };
-    });
+  outputs = inputs: (import "${inputs.substrate}/lib/repo-flake.nix" {
+    inherit (inputs) nixpkgs flake-utils;
+  }) {
+    self = inputs.self;
+    language = "go";
+    builder = "tool";
+    pname = "terraform-provider-akeyless";
+    vendorHash = "sha256-ItO75dB0d4zH09IxNyoG1gJ0NmLge7ml/hFRT0hXsJE=";
+    proxyVendor = true;
+    description = "Akeyless Terraform Provider - manage Akeyless resources via Terraform";
+    homepage = "https://github.com/pleme-io/terraform-provider-akeyless";
+  };
 }
